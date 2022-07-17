5ffd436a1fd630af42bebecafa05ff31d9c61b0e
amyspark-ng <amychan204060@gmail.com>
2022-07-17 14:13:47 -0500

    heheh

---
+++ pre-2.0-ver/code/ApiStuff.js
@@ -0,0 +1,6 @@
+export let NgCore = " "
+export let EncryptionKey = " "
+export let Medal_IDS = [0, 1, 2, 3, 4, 5, 6, 7]
+export let HasMedal = [false, false, false, false, false, false, false, false]
+// ^ Mark!, Easy, Mid, Hard, MAX, Loss, jiji, annoy
+export let ScoreBoard = [0]
